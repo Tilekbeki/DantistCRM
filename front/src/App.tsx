b8ec0d8fd9ed25f1b6233c4794fc5c6157c7fd2d
Tilek <timur.almamatov@yandex.ru>
2025-09-29 02:26:06 +0700

    убрал некоторые файлы из-за которых были ошибки

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import SideBar from './components/SideBar'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
